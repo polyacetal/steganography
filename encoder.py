1caeac0e14b79c9e366c1ef0be586e0ad2cdab37
yugu0202 <applerin1119@gmail.com>
2022-06-30 12:00:14 +0900

    monoIng only PIL

--- encoder.py
+++ encoder.py
@@ -1,23 +1,23 @@
 import sys
 import math
 from PIL import Image
-import numpy as np
+from PIL import ImageFont
+from PIL import ImageDraw
 
 def Main():
     args = sys.argv
     print(args[1])
     print(args[2])
-    img1 = np.array(Image.open(args[1]))
-    img2 = np.array(Image.open(args[2]))
+    img1 = Image.open(args[1])
+    img2 = AutoImageCreate(args[1],args[2])
     img3 = img2
 
-    img_print = Image.fromarray(img1)
-    print("import img success")
-    img_print.show()
-    img1 = MonoImg(img1)
-    img_print = Image.fromarray(img1)
+    #img_print = Image.fromarray(img1)
+    #print("import img success")
+    #img_print.show()
+    img1 = img1.convert('L')
+    img1.show()
     print("mono img success")
-    img_print.show()
     img1 = EvenImg(img1)
     img_print = Image.fromarray(img1)
     print("even img success")
@@ -70,6 +70,29 @@ def MergeImg(img1,img2):
             img3[x,y,1] = img1[x,y,1] + img2[x,y,1]
             img3[x,y,2] = img1[x,y,2] + img2[x,y,2]
     return img3
+
+def AutoImageCreate(image_path,text):
+    # 使うフォント，サイズ，描くテキストの設定
+    ttfontname = "/usr/share/fonts/fonts-go/Go-Mono-Bold.ttf"
+    fontsize = 36
+
+    # 画像サイズ，背景色，フォントの色を設定
+    img = Image.open(image_path)
+    canvasSize = img.size
+    backgroundRGB = (0, 0, 0)
+    textRGB = (255, 255, 255)
+
+    # 文字を描く画像の作成
+    img = Image.new('RGB', canvasSize, backgroundRGB)
+    draw = ImageDraw.Draw(img)
+
+    # 用意した画像に文字列を描く
+    font = ImageFont.truetype(ttfontname, fontsize)
+    textWidth, textHeight = draw.textsize(text,font=font)
+    textTopLeft = (canvasSize[0]//2-textWidth//2, canvasSize[1]//2-textHeight//2) 
+    draw.text(textTopLeft, text, fill=textRGB, font=font)
+
+    return img
 
 if __name__ == "__main__":
     Main()
