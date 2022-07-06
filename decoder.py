2928654105a2bc7aec71433c9b86d02bc317f7ac
yugu0202 <applerin1119@gmail.com>
2022-07-06 10:30:20 +0900

    fast

--- decoder.py
+++ decoder.py
@@ -4,38 +4,17 @@ from PIL import Image
 def Main():
     args = sys.argv
     print(args[1])
-    img1 = Image.open(args[1])
+    img = Image.open(args[1]).convert("L")
 
-    img1 = ModImg(img1)
+    img = img.point(lambda x: x%2)
     print("modulo img success")
-    img1.show()
-    img1.save('appear_key.png')
-    img1 = X255Img(img1)
+    img.show()
+    img.save('appear_key.png')
+    img = img.point(lambda x: x*255).convert("RGB")
     print("x255 img success")
-    img1.show()
-    img1.save('appear_key.png')
+    img.show()
+    img.save('appear_key.png')
 
-def ModImg(img):
-    m, n = img.size
-    for x in range(m):
-        for y in range(n):
-            r, g, b = img.getpixel((x,y))
-            r = r % 2
-            g = g % 2
-            b = b % 2
-            img.putpixel((x,y),(r,g,b))
-    return img
-
-def X255Img(img):
-    m, n = img.size
-    for x in range(m):
-        for y in range(n):
-            r, g, b = img.getpixel((x,y))
-            r = r * 255
-            g = g * 255
-            b = b * 255
-            img.putpixel((x,y),(r,g,b))
-    return img
 
 if __name__ == "__main__":
     Main()
--- encoder.py
+++ encoder.py
@@ -8,94 +8,59 @@ def Main():
     args = sys.argv
     print(args[1])
     print(args[2])
-    img1 = Image.open(args[1]).convert('RGB')
+    img1 = Image.open(args[1])
     img2 = AutoImageCreate(args[1],args[2])
-    img3 = img2
 
     print("import img success")
     img1.show()
-    img1 = MonoImg(img1)
+    img1 = img1.convert("L")
     print("mono img success")
     img1.show()
-    img1 = EvenImg(img1)
+    img1 = img1.point(lambda x: x-1 if x%2 == 1 else x)
     print("even img success")
     img1.show()
-    img2 = OneImg(img2)
-    print("one img success")
-    img2.show()
-    img3 = MergeImg(img1,img2)
+    img1 = MergeImg(img1,img2).convert("RGB")
     print("merge img success")
-    img3.show()
-    img3.save('hiden.png')
+    img1.show()
+    img1.save('hiden.png')
 
-def MonoImg(img):
-    m, n = img.size
-    print(m,n)
-    for x in range(m):
-        for y in range(n):
-            r, g, b = img.getpixel((x,y))
-            gray = math.floor(r*0.299+g*0.587+b*0.114)
-            img.putpixel((x,y),(gray,gray,gray))
-    return img
-
-def EvenImg(img):
-    m, n = img.size
-    for x in range(m):
-        for y in range(n):
-            r, g, b = img.getpixel((x,y))
-            if r % 2 == 1:
-                r -= 1
-                g -= 1
-                b -= 1
-                img.putpixel((x,y),(r,g,b))
-    return img
-
-def OneImg(img):
-    m, n = img.size
-    for x in range(m):
-        for y in range(n):
-            r, g, b = img.getpixel((x,y))
-            r = int(r / 255)
-            g = int(g / 255)
-            b = int(b / 255)
-            img.putpixel((x,y),(r,g,b))
-    return img
 
 def MergeImg(img1,img2):
-    img3 = img1
-    m, n = img1.size
-    for x in range(m):
-        for y in range(n):
-            r1, g1, b1 = img1.getpixel((x,y))
-            r2, g2, b2 = img2.getpixel((x,y))
-            r3 = r1 + r2
-            g3 = g1 + g2
-            b3 = b1 + b2
-            img3.putpixel((x,y),(r3,g3,b3))
-    return img3
+    width,height = img1.size
+    img2data = img2.getdata()
+    for h in range(height):
+        hcache = h * width
+        for w in range(width):
+            if img2data[hcache+w] == 1:
+                pix = img1.getpixel((w,h))
+                img1.putpixel((w,h),pix+1)
+
+    return img1
+
 
 def AutoImageCreate(image_path,text):
     # 使うフォント，サイズ，描くテキストの設定
-    ttfontname = "C:/Windows/Fonts/msgothic.ttc"
+    ttfontname = "/usr/share/fonts/fonts-go/Go-Mono-Bold.ttf"
     fontsize = 100
 
     # 画像サイズ，背景色，フォントの色を設定
     img = Image.open(image_path)
     canvasSize = img.size
-    backgroundRGB = (0, 0, 0)
-    textRGB = (255, 255, 255)
+    background = 0
+    textColor = 1
 
     # 文字を描く画像の作成
-    img = Image.new('RGB', canvasSize, backgroundRGB)
+    img = Image.new('L', canvasSize, background)
     draw = ImageDraw.Draw(img)
 
     # 用意した画像に文字列を描く
     font = ImageFont.truetype(ttfontname, fontsize)
     textWidth, textHeight = draw.textsize(text,font=font)
-    textTopLeft = (canvasSize[0]//2-textWidth//2, canvasSize[1]//2-textHeight//2)
-    draw.text(textTopLeft, text, fill=textRGB, font=font)
+    textTopLeft = (canvasSize[0]//2-textWidth//2, canvasSize[1]//2-textHeight//2) 
+    draw.text(textTopLeft, text, fill=textColor, font=font)
 
     return img
 
 if __name__ == "__main__":
     Main()
+
